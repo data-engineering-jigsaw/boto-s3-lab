dbd6eff7e113b0647b34813a289fb9aa6cc2461f
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-05-23 16:37:32 -0400

    update lab and tests

--- texas-s3/src/s3_client.py
+++ texas-s3/src/s3_client.py
@@ -7,17 +7,9 @@ def read_and_upload(file_name, bucket_name):
     s3.upload_file(file_name, bucket_name, file_name)
 
 def read_data_from(bucket, object):
-    obj = s3.get_object(Bucket=bucket, Key=object)
-    text = obj['Body'].read()
-    data = json.loads(text)
-    return data
+    pass
 
 def store_restaurant_data(restaurants, bucket):
-    for restaurant in restaurants:
-        data = find_receipts(name)
-        file_name = cleaned_name(restaurant)
-        s3.put_object(Body=json.dumps(data),
-        Bucket=bucket,
-        Key=f'{file_name}.json')
+    pass
 
 
