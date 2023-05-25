315037110559a84b4acd45cbf26f08f77f97608f
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-05-25 17:19:16 -0400

    remove code

--- texas-s3/src/s3_client.py
+++ texas-s3/src/s3_client.py
@@ -4,7 +4,7 @@ s3 = boto3.client('s3')
 
 def read_and_upload(file_name, bucket_name):
     # first arg is to read from ......... then write to
-    s3.upload_file(file_name, bucket_name, file_name)
+    
 
 def read_data_from(bucket, object):
     pass
